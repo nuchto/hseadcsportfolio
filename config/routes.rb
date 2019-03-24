2455cc9ca1931a41a3000f072f6e421f0544a027
PaulaUlf <ermilova.p@mail.ru>
2019-03-24 17:47:38 +0300

    bootstrap start

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get 'pages/home'
   devise_for :users
   resources :members
   resources :projects
@@ -6,5 +8,6 @@ Rails.application.routes.draw do
   resources :people
   resources :groups
   resources :posts
+  root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
